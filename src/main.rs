f2a9dd5c22f3454bd5808b253146fac077a4007d
Carlo Sana <carlo.sana@unicredit.eu>
2018-05-10 18:52:35 +0200

    added arg parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate clap;
 extern crate regex;
 extern crate zmq;
 use std::env;
@@ -5,59 +6,7 @@ use regex::Regex;
 use std::vec::Vec;
 use std::io::{self, Read};
 use zmq::{Message, Socket};
-
-fn print_help() {
-    println!("Usage: zmq-cli (PUB|SUB) tcp://url:port");
-}
-static MISSING_ARGUMENTS: &'static str = "missing arguments";
-
-// fn connect_to_socket(String pattern, )
-fn parse_arguments<'a>(args: &'a Vec<String>) -> Result<(zmq::SocketType, &String), &str> {
-    match args.len() {
-        // no args
-        1 => {
-            print_help();
-            Err(MISSING_ARGUMENTS)
-        }
-        2 => {
-            println!("Missing url & port!");
-            print_help();
-            Err(MISSING_ARGUMENTS)
-        }
-        3 => {
-            let pattern: &str = &args[1];
-            let address: &String = &args[2];
-
-            let parsed_pattern = match pattern {
-                "PUB" => Ok(zmq::PUB),
-                "SUB" => Ok(zmq::SUB),
-                _ => {
-                    println!("pattern not supported. Use PUB or SUB.");
-                    Err("Pattern not supported")
-                }
-            };
-
-            let address_regex = Regex::new(r"^tcp://(.*):(.*)$").unwrap();
-            let parsed_address = if address_regex.is_match(address) {
-                Ok(address)
-            } else {
-                Err("Failed to parse address")
-            };
-
-            println!(
-                "Connecting with pattern {} to address {:?}",
-                pattern, parsed_address
-            );
-
-            parsed_pattern.and_then(|pattern| parsed_address.and_then(|addr| Ok((pattern, addr))))
-        }
-        _ => {
-            println!("too many arguments {:?}", args);
-            print_help();
-            Err(MISSING_ARGUMENTS)
-        }
-    }
-}
+use clap::{App, Arg, SubCommand};
 
 fn connect_to_socket(address: &str, pattern: zmq::SocketType) -> Result<zmq::Socket, &str> {
     println!("Connecting to socket");
@@ -68,71 +17,118 @@ fn connect_to_socket(address: &str, pattern: zmq::SocketType) -> Result<zmq::Soc
     Ok(socket)
 }
 
-fn handle_pub(socket: Socket, addr: &str) {
-    println!("{:?}", socket.get_events());
-    socket.bind(addr).expect("failed to bind");
-    let mut buffer = String::new();
-    loop {
-        match io::stdin().read_line(&mut buffer) {
-            Ok(_) => {
-                let mut split = buffer.split(" ");
-                if let Some(key) = split.next() {
-                    if let Some(value) = split.next() {
-                        let mut sanitized = String::pop(&mut String::from(value.clone()));
-                        socket
-                            .send_str(key, zmq::SNDMORE)
-                            .expect("failed to send key");
-                        // flag 0 to indicate it's the last message
-                        socket.send_str(value, 0).expect("Failed to send value");
-                        println!("Sent message with key {} and value {} ", key, value);
+pub mod zmq_helpers {
+
+    pub mod publisher {
+        use std::io::{self, Read};
+        use zmq::{Message, Socket, SNDMORE};
+        pub fn handle_pub(socket: Socket, addr: &str) {
+            println!("{:?}", socket.get_events());
+            socket.bind(addr).expect("failed to bind");
+            let mut buffer = String::new();
+            loop {
+                match io::stdin().read_line(&mut buffer) {
+                    Ok(_) => {
+                        let mut split = buffer.split(" ");
+                        if let Some(key) = split.next() {
+                            if let Some(value) = split.next() {
+                                socket.send_str(key, SNDMORE).expect("failed to send key");
+                                // flag 0 to indicate it's the last message
+                                socket.send_str(value, 0).expect("Failed to send value");
+                                println!("Sent message with key {} and value {} ", key, value);
+                            }
+                        }
+                    }
+                    Err(err) => println!("Err: {:?}", err),
+                }
+                buffer.clear();
+            }
+        }
+
+    }
+
+    pub mod subscriber {
+        use std::io::{self, Read};
+        use zmq::{Message, Socket};
+
+        pub fn handle_sub(socket: Socket, address: &str, parse_as_string: bool) {
+            socket
+                .connect(address)
+                .expect("Failed to connect to address");
+            socket
+                .set_subscribe("".as_bytes())
+                .expect("Failed to subscribe!");
+            loop {
+                let message = socket
+                    .recv_multipart(0)
+                    .expect("Failed to receive multipart message");
+                let mut parts = message.into_iter();
+                if let Some(key) = parts.next() {
+                    if parse_as_string {
+                        if let Some(value) = parts.next() {
+                            println!(
+                                "{:?} - {:?}",
+                                String::from_utf8(key).unwrap(),
+                                String::from_utf8(value).unwrap()
+                            );
+                        }
+                    } else {
+                        println!("key: {:?}", String::from_utf8(key).unwrap());
                     }
                 }
             }
-            Err(err) => println!("Err: {:?}", err),
         }
-        buffer.clear();
     }
 }
 
-fn handle_sub(socket: Socket, address: &str) {
-    socket
-        .connect(address)
-        .expect("Failed to connect to address");
-    socket
-        .set_subscribe("".as_bytes())
-        .expect("Failed to subscribe!");
-    loop {
-        let message = socket
-            .recv_multipart(0)
-            .expect("Failed to receive multipart message");
-        let mut parts = message.into_iter();
-        if let Some(key) = parts.next() {
-            if let Some(value) = parts.next() {
-                println!(
-                    "{:?} - {:?}",
-                    String::from_utf8(key).unwrap(),
-                    String::from_utf8(value).unwrap()
-                );
-            }
-        }
-    }
+struct Configuration<'a> {
+    pattern: &'a str,
+    address: &'a str,
 }
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("Zmq cli")
+        .arg(
+            Arg::with_name("pattern")
+                .required(true)
+                .possible_values(&["PUB", "SUB"])
+                .help("Zmq pattern to use. Supported: [SUB|PUB]"),
+        )
+        .arg(
+            Arg::with_name("address")
+                .required(true)
+                .help("Address you want to connect/bind"),
+        )
+        .get_matches();
 
-    match parse_arguments(&args) {
-        Ok((pattern, addr)) => {
-            let socket = connect_to_socket(addr, pattern).unwrap();
-            match pattern {
-                zmq::PUB => {
-                    handle_pub(socket, addr);
-                }
-                zmq::SUB => {
-                    handle_sub(socket, addr);
-                }
-                _ => println!(),
+    println!("arg matches {:?}", matches);
+
+    let parsed_configuration = matches.value_of("pattern").and_then(|p| {
+        matches.value_of("address").map(|a| Configuration {
+            pattern: p,
+            address: a,
+        })
+    });
+
+    if let Some(Configuration {
+        pattern: patt,
+        address: addr,
+    }) = parsed_configuration{
+        let pattern = match patt {
+            "SUB" => zmq::SUB,
+            "PUB" => zmq::PUB,
+            _ => panic!("impossible patern found"),
+        };
+
+        let socket = connect_to_socket(addr, pattern).unwrap();
+        match pattern {
+            zmq::PUB => {
+                zmq_helpers::publisher::handle_pub(socket, addr);
             }
+            zmq::SUB => {
+                zmq_helpers::subscriber::handle_sub(socket, addr, false);
+            }
+            _ => println!(),
         }
-        Err(err) => println!("Impossible to continue, found {:?}", err),
     }
 }
